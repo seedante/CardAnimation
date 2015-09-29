79a2dd371d7878541056ae6b6b18876cb4129624
seedante <seedante@gmail.com>
2015-09-30 05:11:40 +0800

    Initial Commit

---
+++ CardAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardAnimation
+//
+//  Created by seedante on 15/9/30.
+//  Copyright © 2015年 seedante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
